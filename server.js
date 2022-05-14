1b72c0ac4e94fe54b208d18bf8f9d99c13ec2944
Omar <omarchavezgutierrezdevelasco@gmail.com>
2022-05-14 18:33:32 -0500

    make it better

---
+++ server.js
@@ -0,0 +1,15 @@
+const animals = require("./animals");
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+const port = 3000;
+
+//endpoint prueba de bienvenida
+app.get("/", (req, res) => {
+  res.json({ message: animals });
+});
+
+app.listen(port, () => {
+  console.log("Code Challenge Server Ready!!");
+});
