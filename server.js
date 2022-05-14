c85c136be927faed7326bfa84e92e0125fad6615
Omar <omarchavezgutierrezdevelasco@gmail.com>
2022-05-14 18:39:53 -0500

    cambiamos el puerto

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 
 const app = express();
 app.use(express.json());
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 //endpoint prueba de bienvenida
 app.get("/", (req, res) => {
