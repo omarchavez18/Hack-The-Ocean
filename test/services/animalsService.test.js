4f464df8e1888a64efb819cd233e8f21580ce799
Omar <omarchavezgutierrezdevelasco@gmail.com>
2022-05-14 20:08:41 -0500

    testing de los metodos

---
+++ test/services/animalsService.test.js
@@ -0,0 +1,47 @@
+const animalsService = require("../../services/animalsService");
+const animals = require("../../animals");
+
+//consultar nombres de los animales
+
+describe("obtener lista de los animals.js", () => {
+  test("lista de names", () => {
+    const names = animals.map((animal) => animal.name);
+    const animales = animalsService.filterAnimals(animals);
+    expect(animales).toStrictEqual(names);
+  });
+
+  // lista de los animales segun su type
+  test("lista de animales segun su tipo", () => {
+    const animalType = animals
+      .filter((animal) => animal.type == "Mammal")
+      .map((animal) => animal.name);
+
+    const animalesEspecie = animalsService.filterAnimalsByType(
+      animals,
+      "Mammal"
+    );
+    expect(animalType).toStrictEqual(animalesEspecie);
+  });
+
+  //
+  test("lista de animales que tengan status vulnerable", () => {
+    const statusVulnerable = animals
+      .filter((animal) => animal.status == "Vulnerable")
+      .map((animal) => animal.name);
+
+    const animalesVulnerables = animalsService.filterByStatus(
+      animals,
+      "Vulnerable"
+    );
+    expect(statusVulnerable).toStrictEqual(animalesVulnerables);
+  });
+
+  // test("lista de animales y sus zonas", () => {
+  //   const animalZone = animals
+  //     .filter((animal) => animal)
+  //     .map((animal) => animal.name && animal.zone);
+
+  //   const animalesVulnerables = animalsService.filterByStatus(animals);
+  //   expect(animalZone).toStrictEqual("");
+  // });
+});
