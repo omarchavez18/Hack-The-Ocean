2121cbc54d61bf98baa5adf2c4b621ca2db561d8
Omar <omarchavezgutierrezdevelasco@gmail.com>
2022-05-14 20:38:08 -0500

    creacion de letodos en los controlers

---
+++ controllers/animalsControllers.js
@@ -0,0 +1,21 @@
+const animalsService = require("./../services/animalsService");
+const animals = require("./../animals");
+
+class animalsControllers {
+  static getAnimalsName() {
+    const animales = animals;
+    return animalsService.filterAnimals(animales);
+  }
+
+  static getAnimalsByType(type) {
+    const animales = animals;
+    return animalsService.filterAnimalsByType(animales, type);
+  }
+
+  static getAnimalsByStatus(status) {
+    const animales = animals;
+    return animalsService.filterByStatus(animales, status);
+  }
+}
+
+module.exports = animalsControllers;
