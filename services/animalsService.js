747e4b7b9ff1285304f1b07a373ca40584f97b9a
Omar <omarchavezgutierrezdevelasco@gmail.com>
2022-05-14 20:08:25 -0500

    creacion de metodos que devuelven arrays

---
+++ services/animalsService.js
@@ -0,0 +1,25 @@
+class animalsService {
+  // consultar nombre de los animales
+  static filterAnimals(animals) {
+    const animalsNames = animals.map((animal) => animal.name);
+    return animalsNames;
+  }
+
+  // consultar nombre de los animales mamiferos
+  static filterAnimalsByType(animals, type) {
+    const animalSpecie = animals
+      .filter((animal) => animal.type == type)
+      .map((animal) => animal.name);
+    return animalSpecie;
+  }
+
+  // consultar el nombre de los animales por su status
+  static filterByStatus(animals, status) {
+    const animalsVulnerable = animals
+      .filter((animal) => animal.status == status)
+      .map((animal) => animal.name);
+    return animalsVulnerable;
+  }
+}
+
+module.exports = animalsService;
