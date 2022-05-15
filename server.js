41f49f97364acc2d422abc7254d79d2f4297055b
Omar <omarchavezgutierrezdevelasco@gmail.com>
2022-05-14 22:37:56 -0500

    CORS

--- server.js
+++ server.js
@@ -5,32 +5,38 @@ const animalsService = require("./services/animalsService");
 
 const app = express();
 app.use(express.json());
+
+//cors
+app.use(function (req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET");
+});
 // eslint-disable-next-line no-undef
 const port = process.env.PORT || 3000;
 
 //endpoint prueba de bienvenida
 app.get("/", (req, res) => {
-    res.json({ animals });
+  res.json({ animals });
 });
 
 //endpoint nombre de los animales
 app.get("/animals/name/:name", (req, res) => {
-    const animales = animalsService.filterAnimals(animals, req.params.name);
-    res.json(animales);
+  const animales = animalsService.filterAnimals(animals, req.params.name);
+  res.json(animales);
 });
 
 //endpoint nombre de los animales por su tipo
 app.get("/animals/type/:type", (req, res) => {
-    const animales = animalsService.filterAnimalsByType(animals, req.params.type);
-    res.json(animales);
+  const animales = animalsService.filterAnimalsByType(animals, req.params.type);
+  res.json(animales);
 });
 
 //endpoint nombre de los animales por su status
 app.get("/animals/status/:status", (req, res) => {
-    const animales = animalsService.filterByStatus(animals, req.params.status);
-    res.json(animales);
+  const animales = animalsService.filterByStatus(animals, req.params.status);
+  res.json(animales);
 });
 
 app.listen(port, () => {
-    console.log("Code Challenge Server Ready!!");
+  console.log("Code Challenge Server Ready!!");
 });
